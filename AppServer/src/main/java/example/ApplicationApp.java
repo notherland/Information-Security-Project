2914c2c2c7e481345dbcf888f79e05ffb2b62b95
kssh0222 <kseniia.shirokova@netcracker.com>
2022-12-10 12:07:16 +0300

    Split modules

---
+++ AppServer/src/main/java/example/ApplicationApp.java
@@ -0,0 +1,11 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationApp.class);
+    }
+}
