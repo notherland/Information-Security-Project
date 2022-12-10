ebb671c89ebf97d1559f97848f78054253f2b0c9
kssh0222 <kseniia.shirokova@netcracker.com>
2022-12-10 13:39:21 +0300

    Delete

--- AuthServer/src/main/java/com/example/config/SecurityConfig.java
+++ AuthServer/src/main/java/com/example/config/SecurityConfig.java
@@ -50,7 +50,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
     }
 }
